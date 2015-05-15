d1e4da7ed25afd7d584cdc389b4b0d0b0d18f42d
Dan Motzenbecker <dan@oxism.com>
2015-05-15 00:29:20 -0400

    media helpers module

---
+++ src/cellf/media.cljs
@@ -0,0 +1,30 @@
+(ns cellf.media
+  (:require [cljs.core.async :refer [chan put!]]
+            [clojure.string  :refer [upper-case]]))
+
+(def prefixes ["webkit" "moz" "ms"])
+
+(defn find-native [obj fn-name]
+  (let [cap-name (str (upper-case (first fn-name)) (apply str (rest fn-name)))]
+    (loop [names (cons fn-name (map #(str % cap-name) prefixes))]
+      (when (seq names)
+        (if-let [f (aget obj (first names))]
+          (.bind f obj)
+          (recur (rest names)))))))
+
+
+(defn get-media
+  ([] (get-media {:video true :audio false}))
+  ([opts]
+    (let [c (chan)]
+      (if-let [native-gum (find-native js/navigator "getUserMedia")]
+        (native-gum
+          (clj->js opts)
+          (fn [stream]
+            (put! c {
+              :status :success
+              :data   (.createObjectURL js/URL stream)}))
+          (fn [_]
+            (put! c {:status :error :data :denied})))
+        (put! c {:status :error :data :unsupported}))
+      c)))
