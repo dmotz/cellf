e379cf307a7ed46680fae07f8e5e748bdc77b14b
Dan Motzenbecker <dan@oxism.com>
2015-05-18 19:55:45 -0400

    indentation fix

--- src/cellf/core.cljs
+++ src/cellf/core.cljs
@@ -1,9 +1,9 @@
 (ns ^:figwheel-always cellf.core
-    (:require [om.core :as om :include-macros true]
-              [om.dom :as dom :include-macros true]
-              [cljs.core.async :refer [<! >! take! put! chan timeout]]
-              [cellf.media :as media])
-    (:require-macros [cljs.core.async.macros :refer [go-loop]]))
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [<! >! take! put! chan timeout]]
+            [cellf.media :as media])
+  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
 
 (enable-console-print!)
 
