03ce7352d3d99e2cfcaaca71c519f8444bd54c40
Dan Motzenbecker <dan@oxism.com>
2016-02-06 21:13:48 -0500

    update deps, reformat project.clj

--- project.clj
+++ project.clj
@@ -5,53 +5,43 @@
             :url "http://opensource.org/licenses/mit-license.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.189"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/core.async "0.2.374"]
                  [org.omcljs/om "0.9.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-2"]]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-figwheel "0.5.0-6"]]
 
   :source-paths ["src" "test"]
 
-  :clean-targets ^{:protect false} [
-    "resources/public/js/compiled"
-    "resources/test/compiled.js"
-    "target"]
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "resources/test/compiled.js"
+                                    "target"]
 
-  :cljsbuild {
-    :builds {
-      :dev {
-        :source-paths ["src"]
-        :figwheel {}
-        :compiler {
-          :main cellf.core
-          :asset-path "js/compiled/out"
-          :output-to "resources/public/js/compiled/cellf.js"
-          :output-dir "resources/public/js/compiled/out"
-          :optimizations :none
-          :source-map true
-          :source-map-timestamp true
-          :cache-analysis true}}
+  :cljsbuild {:builds {:dev {:source-paths ["src"]
+                             :figwheel {}
+                             :compiler {:main cellf.core
+                                        :asset-path "js/compiled/out"
+                                        :output-to "resources/public/js/compiled/cellf.js"
+                                        :output-dir "resources/public/js/compiled/out"
+                                        :optimizations :none
+                                        :source-map true
+                                        :source-map-timestamp true
+                                        :cache-analysis true}}
 
-      :min {
-        :source-paths ["src"]
-        :compiler {
-          :output-to "cellf.min.js"
-          :main cellf.core
-          :optimizations :advanced
-          :pretty-print false
-          :externs ["resources/externs/gif.ext.js"]}}
+                       :min {:source-paths ["src"]
+                             :compiler {:output-to "cellf.min.js"
+                                        :main cellf.core
+                                        :optimizations :advanced
+                                        :pretty-print false
+                                        :externs ["resources/externs/gif.ext.js"]}}
 
-      :test {
-        :source-paths ["src" "test"]
-        :notify-command ["phantomjs" "resources/test/test.js" "resources/test/test.html"]
-        :compiler {
-          :output-to "resources/test/compiled.js"
-          :optimizations :simple
-          :pretty-print true}}}
+                       :test {:source-paths ["src" "test"]
+                              :notify-command ["phantomjs" "resources/test/test.js" "resources/test/test.html"]
+                              :compiler {:output-to "resources/test/compiled.js"
+                                         :optimizations :simple
+                                         :pretty-print true}}}
 
-    :test-commands {
-      "test" ["phantomjs" "resources/test/test.js" "resources/test/test.html"]}}
+              :test-commands {"test" ["phantomjs" "resources/test/test.js" "resources/test/test.html"]}}
 
   :figwheel {:css-dirs ["resources/public/css"]})
