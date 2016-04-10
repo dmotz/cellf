aac7fc9f04d862a2011de130ac30b4e28fc863c4
Dan Motzenbecker <dan@oxism.com>
2016-04-10 14:46:12 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.40"]
                  [org.clojure/core.async "0.2.374"]
                  [org.omcljs/om "0.9.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.2"]
-            [lein-figwheel "0.5.0-6"]]
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-figwheel "0.5.2"]]
 
   :source-paths ["src" "test"]
 
