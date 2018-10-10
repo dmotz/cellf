c6a8b9b93b3f33e6362a96df320202f4f2cf758b
Dan Motzenbecker <dan@oxism.com>
2018-10-09 22:38:45 -0400

    updated media ns for newer rtc spec

--- src/cellf/media.cljs
+++ src/cellf/media.cljs
@@ -1,27 +1,15 @@
 (ns cellf.media
   (:require [cljs.core.async :refer [chan put!]]))
 
-(def prefixes ["webkit" "moz" "ms"])
-
-(defn find-native [obj fn-name]
-  (let [cap-name (str (.toUpperCase (first fn-name)) (apply str (rest fn-name)))]
-    (loop [names (cons fn-name (map #(str % cap-name) prefixes))]
-      (when (seq names)
-        (if-let [f (aget obj (first names))]
-          (.bind f obj)
-          (recur (rest names)))))))
-
-
 (defn get-media
   ([]
-   (get-media {:video true :audio false}))
+   (get-media {:video {:facingMode "user"} :audio false}))
   ([opts]
    (let [c (chan)]
-     (if-let [native-gum (find-native js/navigator "getUserMedia")]
-       (native-gum
-         (clj->js opts)
-         #(put! c {:status :success
-                   :data   (.createObjectURL js/URL %)})
-         #(put! c {:status :error :data :denied}))
+     (if (.-mediaDevices js/navigator)
+       (->
+         (.getUserMedia (.-mediaDevices js/navigator) (clj->js opts))
+         (.then  #(put! c {:status :success :data %}))
+         (.catch #(put! c {:status :error :data :denied})))
        (put! c {:status :error :data :unsupported}))
      c)))
